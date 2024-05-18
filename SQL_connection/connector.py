4b66f9d52172268b009e06804b227b78096b59cb
walkerhsu <walkerhsu0808@gmail.com>
2024-05-18 18:02:15 +0800

    🚸 Create SQL Connector class

--- SQL_connection/connector.py
+++ SQL_connection/connector.py
@@ -5,27 +5,34 @@ import mysql.connector
 # make sure that you have your own config file named "sql_config.json" in the same directory
 # the template of sql_config.json can be found in the same directory named "sql_config_template.json"
 
-def get_sql_config():
-    with open('sql_config.json') as f:
-        config = json.load(f)
 
-    host = config['host']
-    user = config['user']
-    passwd = config['passwd']
-    database = config['database']
+class SQLConnector:
+    def __init__(self) -> None:
+        self.get_sql_config()
+        self.create_database()
 
-    return host, user, passwd, database
+    def get_sql_config(self):
+        with open('sql_config.json') as f:
+            config = json.load(f)
+
+        self.host = config['host']
+        self.user = config['user']
+        self.passwd = config['passwd']
+        self.database = config['database']
+
+    def create_database(self):
+        mydb = mysql.connector.connect(user=self.user, password=self.passwd, host=self.host)
+        cursor = mydb.cursor()
+        cursor.execute(f"CREATE DATABASE IF NOT EXISTS {self.database}")
+        cursor.execute(f"SHOW DATABASES")
+        databases = cursor.fetchall()
+        print(databases)
+        cursor.close()
+        mydb.close()
 
 def main():
-    host, user, passwd, database = get_sql_config()
-    mydb = mysql.connector.connect(user=user, password=passwd, host=host)
-    cursor = mydb.cursor()
-    cursor.execute(f"CREATE DATABASE IF NOT EXISTS {database}")
-    cursor.execute(f"SHOW DATABASES")
-    databases = cursor.fetchall()
-    print(databases)
-    cursor.close()
-    mydb.close()
+    connector = SQLConnector()
+    
 
 if __name__ == "__main__":
     main()
